a278cc1cc7d0bc3b5da9510fe31c1246a4e20f35
Anand Akella <akella_anand@yahoo.com>
2022-09-21 18:21:43 -0400

    Samething as before

--- site/Module 4&5 Examples/js/script.js
+++ site/Module 4&5 Examples/js/script.js
@@ -1,38 +1,38 @@
-// var string = "hello";
-// string += " World";
-// string = string + " world";
-// console.log(string + "!");
+var string = "hello";
+string += " World";
+string = string + " world";
+console.log(string + "!");
 
 
 
 
-// console.log((5+4)/3);
-// console.log(undefined/5)
-// console.log(5/undefined)
-// function test1 (a) {
-//     a=15
-//     console.log(a/5);
-// }
-// test1();
+console.log((5+4)/3);
+console.log(undefined/5)
+console.log(5/undefined)
+function test1 (a) {
+    a=15
+    console.log(a/5);
+}
+test1();
 
 
 // equality
-// var x = 4, y = 4;
-// if (x==y) {
-//     console.log("x=4 is equal to y=4")
-// }
-// x = "4"
-// if (x==y) {
-//     console.log("x='4' is equal to y=4")
-// }
+var x = 4, y = 4;
+if (x==y) {
+    console.log("x=4 is equal to y=4")
+}
+x = "4"
+if (x==y) {
+    console.log("x='4' is equal to y=4")
+}
 
 // strict equality
-// if (x === y) {
-//     console.log("Strict: x='4' is equal to y=4");
-// }
-// else {
-//     console.log("strict: x='4' is not equal to y=4");
-// }
+if (x === y) {
+    console.log("Strict: x='4' is equal to y=4");
+}
+else {
+    console.log("strict: x='4' is not equal to y=4");
+}
 
 if (false || null || undefined || "" || 0 || NaN) {
     console.log("This line won't ever excecute");
@@ -42,6 +42,6 @@ else {
 }
 
 
-if (&& true && "hello" && 1 && -1 && "false") {
+if (true && "hello" && 1 && -1 && "false") {
     console.log("All true")
 }
