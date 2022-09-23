7c5e15fa19a2dcd2a630723240b6dc2c274204d4
Anand Akella <akella_anand@yahoo.com>
2022-09-23 16:41:50 -0400

    added lesson 45

--- site/Module 4&5 Examples/js/script2.js
+++ site/Module 4&5 Examples/js/script2.js
@@ -1,8 +1,28 @@
-var company = new Object();
-company.name = "Facebook";
-company.ceo = new Object();
-company.ceo.firstName = "Mark";
-company.ceo.favColor = "Blue";
+// var company = new Object();
+// company.name = "Facebook";
+// company.ceo = new Object();
+// company.ceo.firstName = "Mark";
+// company.ceo.favColor = "Blue";
 
-console.log(company);
-console.log(company.name + "'s CEO's name is " + company.ceo.firstName)
+// console.log(company);
+// console.log(company.name + "'s CEO's name is " + company.ceo.firstName)
+
+// console.log(company.ceo["firstName"])
+// var stockPropName = "stock of company";
+// company[stockPropName] = 110
+
+// console.log("Stocks have gone up until $" + company[stockPropName])
+
+// // Better way for same thing: object literal
+
+var facebook = {
+    name: "Facebook",
+    ceo: {
+        firstName: "Mark",
+        lastName: "Zuckerberg",
+        favColor: "blue"
+    },
+    "stock of company": "$110"
+};
+
+console.log(facebook)       
