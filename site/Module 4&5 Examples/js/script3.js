1ac436d1bca57d2dd9872453697c670ae3b5a15d
Anand Akella <akella_anand@yahoo.com>
2022-09-25 13:52:21 -0400

    fixed html and js

---
+++ site/Module 4&5 Examples/js/script3.js
@@ -0,0 +1,31 @@
+function multiply(x, y) {
+    return x * y
+}
+multiply.version = "v.1.0.0"
+console.log(multiply.version)
+
+
+function makeMultiplier(multiplier) {
+    var myFunc = function (x) {
+        return multiplier * x;
+    }
+
+    return myFunc
+}
+
+var y = 1345
+
+var multiplyBy3 = makeMultiplier(3);
+console.log(multiplyBy3(y))
+var doubleAll = makeMultiplier(2)
+console.log(doubleAll(y))
+
+
+function doOperation(x, operation) {
+    return operation(x)
+}
+
+var result = doOperation(y, multiplyBy3)
+console.log(result)
+result = doOperation(y, doubleAll)
+console.log(result)
