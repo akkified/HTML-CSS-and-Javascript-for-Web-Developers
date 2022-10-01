e3ba1cc57b7f2c443a9d80f257cb159edb5c7c92
Anand Akella <akella_anand@yahoo.com>
2022-10-01 16:01:31 -0400

    starting mod 4

---
+++ site/Module 4&5 Examples/js/script4.js
@@ -0,0 +1,18 @@
+// var array = new Array;
+// array[0] = "akhil"
+// array[1] = 2;
+// array[2] = function (name) { 
+//     console.log("Hello " + name)
+// }
+// array[3] = {course: " HTML, CSS & JS"}
+
+// console.log(array)
+// array[2](array[0])
+
+
+var names = ["Yaakov", "John", "Jones", "Bob"]
+console.log(names)
+
+for (var i = 0; i < names.length; i++) {
+    console.log("Hello " + names[i])
+}
