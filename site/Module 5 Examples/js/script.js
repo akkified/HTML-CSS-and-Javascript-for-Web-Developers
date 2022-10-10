06141a0a0f6b94400d952306da7038d02b9052cb
Anand Akella <akella_anand@yahoo.com>
2022-10-10 15:49:47 -0400

    Added BAM project

--- site/Module 5 Examples/js/script.js
+++ site/Module 5 Examples/js/script.js
@@ -3,11 +3,34 @@
 
 // console.log(document instanceof HTMLDocument);
 
+
 function sayHello () {
    var name = 
     document.getElementById("name").value
-    var message = "<h4>Hello " + name + "!</h4>"
+    var message = "<h4>Dear " + name + ",</h4>"
 
-    document.getElementById("content")
-    .innerHTML = message;
-    }
+      document.getElementById("content").innerHTML = message;
+
+         if (name === "creator") {
+            var title = document.querySelector("#title").textContent
+            title += " created by Akhil the ploppy poopy"
+            var title = document.querySelector("#title").textContent = title
+         }
+      }
+      
+function note () {
+   var note1 = 
+      document.getElementById("note1").value
+      var message1 = "<h4>" + note1 + "!</h4>"
+   
+      document.getElementById("content").innerHTML = message1;   
+   }
+
+
+function sayBye () {
+var name1 = 
+   document.getElementById("name1").value
+   var message1 = "<h4>Lots of love from " + name1 + "!</h4>"
+
+   document.getElementById("content").innerHTML = message1;   
+}
